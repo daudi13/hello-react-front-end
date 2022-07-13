2bfdcc902d37e7b3a8ef4b8c6b49045417ca5efd
daudi13 <davouma13@gmail.com>
2022-07-13 14:11:41 +0300

    configure Greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getGreeting } from '../store/greetings';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const randomGreeting = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, []);
+
+  return (
+    <div className="box">
+      <p className="text">{randomGreeting.message}</p>
+    </div>
+  );
+};
+
+Greeting.propTypes = {
+  message: PropTypes.string,
+};
+export default Greeting;
